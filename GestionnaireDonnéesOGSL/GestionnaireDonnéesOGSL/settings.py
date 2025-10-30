70260aaff4f6b64e881104ba59cbd1191224d3cc
Ivan Kenfack <kenfackzeuns@gmail.com>
2025-10-29 23:13:11 -0400

    Add REST API integration and update models for improved data handling

--- GestionnaireDonnéesOGSL/GestionnaireDonnéesOGSL/settings.py
+++ GestionnaireDonnéesOGSL/GestionnaireDonnéesOGSL/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalogueDonnées.apps.CataloguedonnéesConfig',
+    'rest_framework',
+    'apiREST.apps.ApirestConfig',
 ]
 
 MIDDLEWARE = [
--- GestionnaireDonnéesOGSL/catalogueDonnées/models.py
+++ GestionnaireDonnéesOGSL/catalogueDonnées/models.py
@@ -6,12 +6,12 @@ class Jeu_De_Donnée(models.Model):
 
     idJeuDeDonnée = models.AutoField(primary_key=True)
     organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
-    auteur = models.CharField(max_length=500, blank=True)
+    auteur = models.CharField(max_length=500, null=True,blank=True)
     date_création = models.DateTimeField(null=True, blank=True)
-    email_auteur = models.CharField(max_length=500, blank=True)
-    url_licence = models.CharField(max_length=500,blank=True)
+    email_auteur = models.CharField(max_length=500, blank=True,null=True,)
+    url_licence = models.CharField(max_length=500,blank=True,null=True,)
     date_création_métadonnées = models.DateTimeField(null=True,blank=True)
-    nom = models.CharField(max_length=500,blank=True)
+    nom = models.CharField(max_length=500,blank=True,null=True,)
     nombre_ressources = models.IntegerField(null=True,blank=True)
     nombre_mots_clés = models.IntegerField(null=True,blank=True)
 
@@ -23,13 +23,13 @@ class Ressource(models.Model):
     
     idRessource = models.AutoField(primary_key=True)
     jeu_de_donnée = models.ForeignKey('Jeu_De_Donnée', on_delete=models.CASCADE)
-    date_création = models.DateTimeField(6,null=True, blank=True)
-    description = models.TextField(2000,blank=True)
+    date_création = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(2000,blank=True,null=True,)
     format_ressource = models.CharField(max_length=50,blank=True)
     dernière_modification = models.DateTimeField(null=True, blank=True)
-    nom = models.CharField(max_length=500,blank=True)
-    type_ressource = models.CharField(max_length=100,blank=True)
-    url  = models.CharField(max_length=500,blank=True)
+    nom = models.CharField(max_length=500,blank=True,null=True,)
+    type_ressource = models.CharField(max_length=100,blank=True,null=True,)
+    url  = models.CharField(max_length=500,blank=True,null=True,)
     taille_ressource = models.IntegerField(null=True,blank=True)
     
     def __str__(self):
@@ -40,9 +40,9 @@ class Mot_Clé(models.Model):
 
     idMotClé = models.AutoField(primary_key=True)
     jeu_de_donnée = models.ForeignKey('Jeu_De_Donnée', on_delete=models.CASCADE)
-    nom_dAffichage = models.CharField(max_length=100,blank=True)
-    mot_clé = models.CharField(max_length=100,blank=True)
-    état = models.CharField(max_length=45,blank=True)
+    nom_dAffichage = models.CharField(max_length=100,blank=True,null=True,)
+    mot_clé = models.CharField(max_length=100,blank=True,null=True,)
+    état = models.CharField(max_length=45,blank=True,null=True,)
 
     def __str__(self):
         return self.mot_clé
@@ -51,10 +51,10 @@ class Mot_Clé(models.Model):
 class Organisation(models.Model):
 
     idOrganisation = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=500,blank=True)
-    titre = models.CharField(max_length=500,blank=True)
-    statut_dApprobation = models.CharField(max_length=45,blank=True)
-    état = models.CharField(max_length=45,blank=True)
+    nom = models.CharField(max_length=500,blank=True,null=True,)
+    titre = models.CharField(max_length=500,blank=True,null=True,)
+    statut_dApprobation = models.CharField(max_length=45,blank=True,null=True,)
+    état = models.CharField(max_length=45,blank=True,null=True,)
    
     def __str__(self):
         return self.nom
@@ -64,9 +64,9 @@ class Group(models.Model):
 
     idGroupe = models.AutoField(primary_key=True)
     jeu_de_donnée = models.ForeignKey('Jeu_De_Donnée', on_delete=models.CASCADE)
-    description = models.TextField(1000,blank=True)
-    nom = models.CharField(max_length=500,blank=True)
-    titre = models.CharField(max_length=500,blank=True)
+    description = models.TextField(1000,blank=True,null=True,)
+    nom = models.CharField(max_length=500,blank=True,null=True,)
+    titre = models.CharField(max_length=500,blank=True,null=True,)
 
     def __str__(self):
         return self.nom
