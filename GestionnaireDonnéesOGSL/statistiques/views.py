5fb57503bc1f1381cdffe223fc28757be2acc2e0
Ivan Kenfack <kenfackzeuns@gmail.com>
2025-11-02 18:42:43 -0500

    Implement view logic and update templates for statistiques app

--- GestionnaireDonnéesOGSL/statistiques/views.py
+++ GestionnaireDonnéesOGSL/statistiques/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Count
+from catalogueDonnées.models import Jeu_De_Donnee, Group
+import json
 
 def index(request):
-    return render(request, 'index.html')
+    thematiques = Group.objects.values_list('nom', flat=True).distinct()
+    thematiques = list(thematiques)
+
+    nombreJeuxParThematique = Group.objects.values('nom').annotate(total = Count('pk'))
+    nombreJeuxParThematique = list(nombreJeuxParThematique)
+
+    context = {
+        'thematiques' : json.dumps(thematiques),
+        'nombreJeuxParThematique': json.dumps(nombreJeuxParThematique)
+    }
+
+    return render(request, 'index.html', context)
